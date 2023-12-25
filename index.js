97b9d6982b0fa83e292a09a97c0d158079adde11
atheo810 <atheos810@gmail.com>
2023-12-25 18:22:00 +0800

    feat: limit up to 50mb

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const router = require("./routers");
 // using depedencies
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(router);
 
