f1a0f104a84a4636e7eb67fa23a2183c0df83cd8
atheo810 <atheos810@gmail.com>
2023-12-25 16:00:54 +0800

    feat: seeder

---
+++ prisma/seed.js
@@ -0,0 +1,48 @@
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("../lib/bcrypt");
+const { generateHash } = bcrypt;
+
+const prisma = new PrismaClient();
+
+const users = [
+  {
+    name: "atheo",
+    email: "atheos810@gmail.com",
+    password: generateHash("admin"),
+  },
+  {
+    name: "rangga",
+    email: "rangga@gmail.com",
+    password: generateHash("admin"),
+  },
+  {
+    name: "nara",
+    email: "nara@gmail.com",
+    password: generateHash("admin"),
+  },
+  {
+    name: "adi",
+    email: "adi@gmail.com",
+    password: generateHash("admin"),
+  },
+];
+
+async function main() {
+  for (const user of users) {
+    await prisma.user.create({
+      data: user,
+    });
+  }
+
+  console.log("Seed data success");
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
