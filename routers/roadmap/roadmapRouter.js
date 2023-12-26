b17b07bce0cd781e4096da6e747344ba24ae9eb1
atheo810 <atheos810@gmail.com>
2023-12-26 23:50:22 +0800

    feat: roadmapRouter getById & All

--- routers/roadmap/roadmapRouter.js
+++ routers/roadmap/roadmapRouter.js
@@ -6,8 +6,15 @@ const router = Router();
 
 // use userController
 const roadmapController = require("../../controllers/roadmapController/roadmapController");
-const { createRoadmap, relationRoadmapToCategory } = roadmapController;
+const {
+  createRoadmap,
+  relationRoadmapToCategory,
+  getAllRoadmap,
+  getRoadmapById,
+} = roadmapController;
 
+router.get("/roadmap", getAllRoadmap);
+router.get("/roadmap/:id", getRoadmapById);
 router.post("/roadmap", createRoadmap);
 router.post("/roadmap/:id", relationRoadmapToCategory);
 module.exports = router;
