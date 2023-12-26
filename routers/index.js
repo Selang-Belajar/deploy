08e41a5b0de00f27b8b3272b16e134bb1389119a
atheo810 <atheos810@gmail.com>
2023-12-26 23:08:59 +0800

    feat: user category & roadmap routers

--- routers/index.js
+++ routers/index.js
@@ -2,12 +2,16 @@
 const { Router } = require("express");
 // use userRouter from user folder
 const userRouter = require("./user/userRouter");
+const categoryRouter = require("./category/categoryRouter");
+const roadmapRouter = require("./roadmap/roadmapRouter");
 
 // define router from Router()
 const router = Router();
 
 // router use userRouter
 router.use(userRouter);
+router.use(categoryRouter);
+router.use(roadmapRouter);
 
 // export router for use in index.js ( main )
 module.exports = router;
