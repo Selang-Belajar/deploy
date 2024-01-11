d6f80cbde45fbdcf61c14a2cb69739aee90b15e9
atheo810 <atheos810@gmail.com>
2024-01-11 23:50:03 +0800

    feat: tokenController

---
+++ controllers/tokenController/tokenController.js
@@ -0,0 +1,16 @@
+// user PrismaClient from @prisma/client
+const { PrismaClient } = require("@prisma/client");
+// define prisma making new Class PrismaClient
+const prisma = new PrismaClient();
+
+class tokenController {
+    static async createToken(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
+
+export default tokenController;
